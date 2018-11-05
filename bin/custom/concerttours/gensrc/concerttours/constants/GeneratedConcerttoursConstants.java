4578b7d322864b9953b017b38986905f5f65a9db
Wricked <victorbologna@hotmail.com>
2018-11-05 11:45:52 -0200

    Add your Extension

---
+++ bin/custom/concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 05/11/2018 11:17:52                         ---
+ * ----------------------------------------------------------------
+ */
+package concerttours.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedConcerttoursConstants
+{
+	public static final String EXTENSIONNAME = "concerttours";
+	
+	protected GeneratedConcerttoursConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
